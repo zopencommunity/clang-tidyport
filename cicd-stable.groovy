d478cac93892cbdde50a35848c5164dd83031d4a
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2025-11-25 12:48:55 -0500

    Update cicd-stable.groovy
    Signed-off-by: Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/clang-formatport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/clang-tidyport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/clang-formatport.git'), string(name: 'PORT_DESCRIPTION', value: 'the llvm clang format tool' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/clang-tidyport.git'), string(name: 'PORT_DESCRIPTION', value: 'the llvm clang tidy tool' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
